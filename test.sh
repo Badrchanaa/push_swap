fc1de1bd86f479e606f347d1d9490e4ecf4d4631
Badr Chanaa <bchanaa@c4r4p9.1337.ma>
2024-03-26 02:56:21 +0000

    test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 max=$(($1 - 1))
 arg="$(shuf -i 0-$max)"
 echo $arg | tr ' ' ', '
-./push_swap $arg > out.txt
+valgrind --log-file=valgrind.txt ./push_swap $arg > out.txt
 echo "Number of ops: "; cat out.txt | wc -l
 ./checker_linux $arg < out.txt
+vl=$(cat valgrind.txt | grep "No leaks are possible" | wc -l)
+echo $vl
+if [$vl -gt 0] then
+	echo "No leaks"
+
+	echo "Leaks detected"
+
